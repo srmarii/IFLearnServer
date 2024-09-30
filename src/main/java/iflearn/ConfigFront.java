84f0d4186528df9eecb0f86a2d18d09fb6f5b86a
Mari Ramos <marisr2005@gmail.com>
2024-09-29 21:45:59 -0300

    movi a pasta de configurações do front pra pasta main

--- src/main/java/iflearn/ConfigFront.java
+++ src/main/java/iflearn/ConfigFront.java
@@ -1,3 +1,4 @@
+package iflearn;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
